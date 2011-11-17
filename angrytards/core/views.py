160dd17e36a70c4ed362d1f0f68dc499ccef6fe9
Giovanni Collazo <gcollazo@gmail.com>
2011-11-16 23:15:51 -0400

    added initial comment support

--- angrytards/core/views.py
+++ angrytards/core/views.py
@@ -1,4 +1,4 @@
-from BeautifulSoup import BeautifulStoneSoup
+from BeautifulSoup import BeautifulStoneSoup, BeautifulSoup
 from django.http import HttpResponse
 import urllib2
 import json
@@ -15,19 +15,35 @@ def stories(request):
 
 	stories = []
 	for item in soup.findAll('item'):
+		story_id = item.guid.contents[0].split('/')[-1].split('.')[0].split('-')[-1]
 		stories.append({
 			'title':item.title.contents[0],
 			'url':item.guid.contents[0],
-			'id':item.guid.contents[0].split('/')[-1].split('.')[0].split('-')[-1],
-			'comments':'/story/%s/comments/' % item.guid.contents[0].split('/')[-1].split('.')[0].split('-')[-1]
+			'id':story_id,
+			'comments':'http://%s/story/%s/comments/' % (request.META['HTTP_HOST'], story_id)
 		})
 
 	return HttpResponse(json.dumps(stories), mimetype='application/json')
 
-def comments(request, story_id, page=None):
-	base_url = 'http://www.elnuevodia.com/XStatic/endi/template/cargaListaComentarios.aspx?intConfigurationId=12275&intElementId=%s'
-	print story_id
-	print page
-	print
+def comments(request, story_id, page=1):
+	base_url = 'http://www.elnuevodia.com/XStatic/endi/template/cargaListaComentarios.aspx?intConfigurationId=12275&intElementId=%s&p=%s'
+	
+	opener = urllib2.build_opener()
+	opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+	f = opener.open(base_url % (story_id, page))
+	
+	soup = BeautifulSoup(f.read())
 
-	return HttpResponse('Comments')
+	comentarios = soup.findAll('div', attrs={'class': 'comentarios'})
+
+	comments = []
+	for c in comentarios:
+		username = c.findAll('h2')[0].contents[-1].replace('\r\n        ', '').replace('\r\n        ', '')
+		comment = c.findAll('p', attrs={'class': 'copete clearfix'})[0].contents[0].replace('\r\n        ', '')
+		
+		comments.append({
+			'username':username,
+			'comment':comment
+		})
+		
+	return HttpResponse(json.dumps(comments), mimetype='application/json')
