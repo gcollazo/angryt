c01b3b9e5b1d7f10bba79f7c3ac7f6e675bd48f1
Giovanni Collazo <gcollazo@gmail.com>
2011-11-20 16:01:20 -0400

    removed Procfile and gunicorn

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: python angrytards/manage.py run_gunicorn -b "0.0.0.0:$PORT" -w 3
--- angrytards/settings.py
+++ angrytards/settings.py
@@ -120,7 +120,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'gunicorn',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
