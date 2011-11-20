2ac10e393ce5eca01fabfc31a734e8394c59181b
Giovanni Collazo <gcollazo@gmail.com>
2011-11-20 15:59:42 -0400

    Removed static file finder for filesystem

--- angrytards/settings.py
+++ angrytards/settings.py
@@ -81,7 +81,7 @@ STATICFILES_DIRS = (
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
+    # 'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
