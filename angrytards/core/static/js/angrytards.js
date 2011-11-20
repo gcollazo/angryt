c789d732066d238de4631c1a6973359cfa6bbdb1
Giovanni Collazo <gcollazo@gmail.com>
2011-11-20 18:02:21 -0400

    Added style to make the thing look kind of nice

--- angrytards/core/static/js/angrytards.js
+++ angrytards/core/static/js/angrytards.js
@@ -3,20 +3,49 @@ $(document).ready(function(){
     var itemTemplate = _.template($("#story-template").html());
     var commentTemplate = _.template($("#comment-template").html());
 
-    $(".comment-url").live('click', function(e){
-        e.preventDefault();
-        var url = $(e.currentTarget).attr('href');
-        $(e.currentTarget).after('<ul></ul>');
-        $.getJSON(url, function(data){
-            $(e.currentTarget).parent().find('ul').append(commentTemplate(data));
-        });
-    });
+    // Templates
+    var largeStoryTemplate = _.template($("#large-story-template").html());
+    var mediumStoryTemplate = _.template($("#medium-story-template").html());
+    var smallStoryTemplate = _.template($("#small-story-template").html());
+
+    // Insertion point
+    var app = $("#news");
+
+    // $(".comment-url").live('click', function(e){
+    //     e.preventDefault();
+    //     var url = $(e.currentTarget).attr('href');
+    //     $(e.currentTarget).after('<ul></ul>');
+    //     $.getJSON(url, function(data){
+    //         $(e.currentTarget).parent().find('ul').append(commentTemplate(data));
+    //     });
+    // });
 
 
-    $.getJSON('/story/', function(data) {
-        for(var i=0; i<data.length; i++){
-            $("#app").append(itemTemplate(data[i]));
+    $.getJSON('/story/', gotDataCallback);
+
+    var secondRow = {stories:[]};
+    var otherRows = {stories:[]};
+
+    function gotDataCallback(data){
+        for(var i = 0; i < data.length; i++){
+            
+            if(i === 0){
+                app.append(largeStoryTemplate(data[i]));
+            } else if(i > 0 && i < 4) {
+                secondRow.stories.push(data[i]);
+                if(secondRow.stories.length === 3){
+                    app.append(mediumStoryTemplate(secondRow));
+                }
+            } else {
+                otherRows.stories.push(data[i]);
+                if(otherRows.stories.length === 4) {
+                    app.append(smallStoryTemplate(otherRows));
+                    otherRows.stories = [];
+                }
+            }
+
+
         }
-    });
+    }
 
 });
